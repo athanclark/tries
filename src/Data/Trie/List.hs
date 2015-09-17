5c1b4055ebe2a7bc5169b7ab7ce228355d46ad87
Athan Clark <athan.clark@gmail.com>
2015-09-16 23:52:29 -0600

    unneeded module

--- src/Data/Trie/List.hs
+++ src/Data/Trie/List.hs
@@ -1,13 +1,30 @@
+{-# LANGUAGE
+    TypeFamilies
+  , DeriveFunctor
+  , DeriveFoldable
+  , DeriveTraversable
+  , GeneralizedNewtypeDeriving
+  #-}
+
 module Data.Trie.List where
 
 import Prelude hiding (lookup)
-import           Data.Trie.List.Internal
 import           Data.Tree (Tree (..))
 import           Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NE
 
+import Data.Key hiding (lookup)
 import Data.Monoid
 import Control.Monad
+
+
+newtype ListTrie t x = ListTrie
+  { unListTrie :: Tree (t, Maybe x)
+  } deriving (Show, Eq, Functor, Foldable, Traversable)
+
+type instance Key (ListTrie t) = NonEmpty t
+-- TODO: Trie instance
+
 
 
 lookup :: Eq t => NonEmpty t -> ListTrie t x -> Maybe x
