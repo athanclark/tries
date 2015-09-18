4ece070dc0be14c6786fe747a692d704efe13f08
Athan Clark <athan.clark@gmail.com>
2015-09-17 22:38:47 -0600

    better profile

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,31 +1,59 @@
 module Main where
 
+import           Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NE
+import Data.Tree (Tree (..))
 import Data.Trie.List as L
+import Data.Trie.Map as M
+import Data.Trie.Class as TC
+import qualified Data.Map as Map
 import Criterion.Main
+import Control.Monad.State
 
 
-listTrie = ListTrie $
-  Node (1, Just 1)
-    [ Node (2, Just 2)
-      [ Node (3, Just 3)
-        [ Node (4, Just 4)
-          [ Node (5, Just 5) []
-          ]
-        ]
-      ]
-    ]
+genListTrie :: Int -> ListTrie Int Int
+genListTrie n = ListTrie $ Node (0, Just 0) $ genTree n
+  where
+    genTree :: Int -> [Tree (Int, Maybe Int)]
+    genTree n = evalState (replicateM n $ do
+      i <- getSucc
+      return $ Node (i, Just i) $ genTree (n-1)) 1
+
+
+getSucc :: State Int Int
+getSucc = get <* modify (+1)
+
+
+genMapTrie :: Int -> MapTrie Int Int
+genMapTrie n = MapTrie $ MapStep $ Map.singleton 0
+  (Just 0, Just $ MapTrie $ MapStep $ genMapTree n)
+  where
+    genMapTree :: Int -> Map.Map Int (Maybe Int, Maybe (MapTrie Int Int))
+    genMapTree n = Map.unions $ evalState (replicateM n $ do
+      i <- getSucc
+      return $ Map.singleton i ( Just i
+                               , if n <= 1 then Nothing
+                                 else Just $ MapTrie $ MapStep $ genMapTree (n-1))) 1
 
 
 
 main = defaultMain
   [ bgroup "lookup"
     [ bgroup "ListTrie"
-      [ bench "1" $ whnf (L.lookup $ NE.fromList [1])         listTrie
-      , bench "2" $ whnf (L.lookup $ NE.fromList [1,2])       listTrie
-      , bench "3" $ whnf (L.lookup $ NE.fromList [1,2,3])     listTrie
-      , bench "4" $ whnf (L.lookup $ NE.fromList [1,2,3,4])   listTrie
-      , bench "4" $ whnf (L.lookup $ NE.fromList [1,2,3,4,5]) listTrie
+      [ bench "1" $ whnf (L.lookup $ 0 :| [100])         (genListTrie 100)
+      , bench "2" $ whnf (L.lookup $ 0 :| [100,99..81])  (genListTrie 100)
+      , bench "3" $ whnf (L.lookup $ 0 :| [100,99..61])  (genListTrie 100)
+      , bench "4" $ whnf (L.lookup $ 0 :| [100,99..41])  (genListTrie 100)
+      , bench "5" $ whnf (L.lookup $ 0 :| [100,99..21])  (genListTrie 100)
+      , bench "6" $ whnf (L.lookup $ 0 :| [100,99..1])   (genListTrie 100)
+      ]
+    , bgroup "MapTrie"
+      [ bench "1" $ whnf (TC.lookup $ 0 :| [100])         (genMapTrie 100)
+      , bench "2" $ whnf (TC.lookup $ 0 :| [100,99..81])  (genMapTrie 100)
+      , bench "3" $ whnf (TC.lookup $ 0 :| [100,99..61])  (genMapTrie 100)
+      , bench "4" $ whnf (TC.lookup $ 0 :| [100,99..41])  (genMapTrie 100)
+      , bench "5" $ whnf (TC.lookup $ 0 :| [100,99..21])  (genMapTrie 100)
+      , bench "6" $ whnf (TC.lookup $ 0 :| [100,99..1])   (genMapTrie 100)
       ]
     ]
   ]
